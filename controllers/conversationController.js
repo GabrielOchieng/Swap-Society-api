3c4d5201d5a1d42336d18eb84c7782ccc9e92d62
GabrielOchieng <ogingagabriel@gmail.com>
2024-07-26 08:33:56 +0300

    modification of create conversation function to avoid duplicates

--- controllers/conversationController.js
+++ controllers/conversationController.js
@@ -1,16 +1,31 @@
 import Conversation from "../models/Conversation.js";
 
 // new conversation
+
 const createConversation = async (req, res) => {
-  const newConversation = new Conversation({
-    members: [req.body.senderId, req.body.receiverId],
+  const senderId = req.body.senderId;
+  const receiverId = req.body.receiverId;
+
+  // Check if a conversation already exists with these members
+  const existingConversation = await Conversation.findOne({
+    members: { $all: [senderId, receiverId] },
   });
 
+  if (existingConversation) {
+    console.log("Conversation already exists:", existingConversation._id);
+    return res.status(200).json({ message: "Conversation already exists" });
+  }
+
   try {
+    const newConversation = new Conversation({
+      members: [senderId, receiverId],
+    });
     const savedConversation = await newConversation.save();
     res.status(200).json(savedConversation);
+    console.log("New conversation created successfully", savedConversation);
   } catch (error) {
-    res.status(500).json(err);
+    res.status(500).json(error);
+    console.log(error);
   }
 };
 
